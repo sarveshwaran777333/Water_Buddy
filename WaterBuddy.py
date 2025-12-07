a804cf4a255b62c1053604d17c95c27f36464547
1000411_K_Sarveshwaran <sarveshwaran@jainvidyalaya.in>
2025-12-07 11:14:21 +0530

    Update WaterBuddy.py

--- WaterBuddy.py
+++ WaterBuddy.py
@@ -328,7 +328,7 @@ def view_settings(uid, profile):
 def view_runner_game():
     st.header("WaterBuddy Runner Game")
 
-    with open("robo.png", "rb") as f:
+    with open("assests/ROBO.png", "rb") as f:
         img = base64.b64encode(f.read()).decode()
 
     html = f"""
